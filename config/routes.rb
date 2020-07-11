648750e2eba1a6102896212fbe77f35064053386
Andy Lee <freerangeandy@gmail.com>
2020-07-11 18:15:50 -0400

    protected routes from forgery; added games show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   root 'static#index'
 
   get '/title', to: 'static#index'
-  get '/lobby', to: 'static#index'
-  get '/games/:id', to: 'static#index'
-  get '/victory', to: 'static#index'
 
   namespace :v1 do
     resources :users, only: [:create, :show]
-    resources :games, only: [:create]
+    resources :games, only: [:create, :show]
   end
 end
