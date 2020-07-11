9a6c886b28ec0679772bee4c4fc597c0d2d8c646
flashnej <flashnej@gmail.com>
2020-07-11 13:38:11 -0400

    added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'static#index'
-  
+
   get '/', to: 'static#index'
- 
+
   namespace :v1, defaults: { format: 'json' } do
     get 'users', to: 'users#index'
+    resources :games, only: [:create]
   end
 end
