ba9b39c47b0aba3e43b491ffb527a8f97a56ae19
Andy Lee <freerangeandy@gmail.com>
2020-07-11 17:51:19 -0400

    added seed file with 1 game, 2 players

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+game1 = Game.create({ passcode: '12345678', current_castle: nil })
+host1 = User.new({ screen_id: 'host1', soldiers_remaining: 100, sent_soldiers: nil, castle_points: 0 })
+guest1 = User.new({ screen_id: 'guest1', soldiers_remaining: 100, sent_soldiers: nil, castle_points: 0 })
+
+host1.game = game1
+guest1.game = game1
+host1.save
+guest1.save
