a43c7597e4f8ad926bfa7b6e125cd6a86bae6dd1
Andy Lee <freerangeandy@gmail.com>
2020-07-11 13:02:12 -0400

    wrapped App component in BrowserRouter; made static GameContainer component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import GameContainer from '../containers/GameContainer'
+
 export const App = (props) => {
   return (
     <div>
       hello world
+      <GameContainer />
     </div>
   )
 }
---
+++ app/javascript/containers/GameContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+const GameContainer = (props) => {
+
+  return (
+    <div>
+      game container
+
+    </div>
+  )
+}
+
+export default GameContainer
