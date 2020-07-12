253116f8085cfacd11bffb965679d0978f6fc8bd
Andy Lee <freerangeandy@gmail.com>
2020-07-12 19:44:48 -0400

    installed redis; added boilerplate ActionCable files

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'static#index'
 
   get '/title', to: 'static#index'
-
+  mount ActionCable.server, at: '/cable'
+  
   namespace :v1 do
     resources :users, only: [:create, :update, :show]
     get '/games/:id/:user_id' => 'games#join'
